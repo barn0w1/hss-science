ea26e5b4a18c55b041b25f0b88eb5e59f9b0e9ac
yuitokiuchi <yuito.kiuchi.dev@gmail.com>
2026-02-04 12:35:32 +0900

    feat: remove authentication-related components

--- web/apps/chat/src/app/App.tsx
+++ web/apps/chat/src/app/App.tsx
@@ -1,11 +1,8 @@
-import { AuthProvider } from '../features/auth/model/AuthProvider';
 import { AppRouter } from './router';
 
 function App() {
   return (
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <AppRouter />
   );
 }
 
