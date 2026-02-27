062411c8f7499692d52c0352a700953d40de10fb
yuitokiuchi <yuito.kiuchi.dev@gmail.com>
2026-02-27 17:00:19 +0900

    refactor: remove profile and security features

--- web/apps/myaccount/src/shared/ui/LoadingSpinner.tsx
+++
@@ -1,5 +0,0 @@
-export const LoadingSpinner = () => (
-  <div className="flex items-center justify-center p-4">
-    <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-200 border-t-blue-600" />
-  </div>
-);
